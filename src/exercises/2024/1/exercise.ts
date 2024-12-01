8c1d6c3f2119fd83df4c1c4df279532a8678092c
Léo Ladevie <lad.leo@hotmail.fr>
2024-12-01 08:00:16 +0100

    Add exercise 1 part 1

--- src/exercises/2024/1/exercise.ts
+++ src/exercises/2024/1/exercise.ts
@@ -1,13 +1,42 @@
 import {Exercise} from '../../exercise';
+import {input} from './input.ts';
 
 export class Exercise12024 extends Exercise {
     aocDay: number = 1;
     aocYear: number = 2024;
-    aocName: string = 'N/C';
+    aocName: string = 'Historian Hysteria';
 
+    input: [number, number][] = input.split('\n')
+        .map((line: string) => {
+            const parts: string[] = line.split('   ');
+            if (parts.length !== 2) {
+                throw new Error('Bad input or parsing');
+            }
+            return parts.map((part: string) => parseInt(part.trim())) as [number, number]
 
-    getPart1Result(): string {
-        return 'Not implemented yet';
+        });
+
+    getPart1Result(): number {
+        // Split input in two array : left and right
+        const leftList: number[] = [];
+        const rightList: number[] = [];
+        this.input.forEach(([left, right]: [number, number]) => {
+            leftList.push(left);
+            rightList.push(right);
+        });
+
+        // Sort arrays
+        leftList.sort((a, b) => a - b);
+        rightList.sort((a, b) => a - b);
+
+        const distances: number[] = leftList.map((leftValue: number, index: number) => {
+            const rightValue: number = rightList[index];
+            return Math.abs(leftValue - rightValue);
+        });
+
+        return distances.reduce((previousValue: number, currentValue: number) => {
+            return previousValue + currentValue
+        }, 0);
     }
 
     getPart2Result(): string {
