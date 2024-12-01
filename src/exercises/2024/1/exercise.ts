7a2c553f544b71b68a1b4c763892793e8c3ec794
Léo Ladevie <lad.leo@hotmail.fr>
2024-12-01 08:26:17 +0100

    Add exercise 1 part 2

--- src/exercises/2024/1/exercise.ts
+++ src/exercises/2024/1/exercise.ts
@@ -29,17 +29,36 @@ export class Exercise12024 extends Exercise {
         leftList.sort((a, b) => a - b);
         rightList.sort((a, b) => a - b);
 
+        // Get distances
         const distances: number[] = leftList.map((leftValue: number, index: number) => {
             const rightValue: number = rightList[index];
             return Math.abs(leftValue - rightValue);
         });
 
+        // Sum
         return distances.reduce((previousValue: number, currentValue: number) => {
             return previousValue + currentValue
         }, 0);
     }
 
-    getPart2Result(): string {
-        return 'Not implemented yet';
+    getPart2Result(): number {
+        // Split input in two array : left and right
+        const leftList: number[] = [];
+        const rightList: number[] = [];
+        this.input.forEach(([left, right]: [number, number]) => {
+            leftList.push(left);
+            rightList.push(right);
+        });
+
+        // Get similarity scores for right values
+        const similarityScores: {[key in number]: number} = rightList.reduce<{[key in number]: number}>((prev, curr: number) => {
+            prev[curr] = curr + (prev[curr] ?? 0);
+            return prev;
+        }, {});
+
+        // Iterate over left list to get similarity scores and sum them
+        return leftList.map((value: number) => {
+            return similarityScores[value] ?? 0;
+        }).reduce((prev, curr) => prev + curr, 0);
     }
 }
