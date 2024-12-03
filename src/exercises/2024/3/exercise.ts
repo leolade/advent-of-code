c6eadcf38803acb08e428102977a0c1e1462ea71
Léo Ladevie <lad.leo@hotmail.fr>
2024-12-03 07:21:06 +0100

    Add exercise 3 part 2

--- src/exercises/2024/3/exercise.ts
+++ src/exercises/2024/3/exercise.ts
@@ -18,7 +18,34 @@ export class Exercise32024 extends Exercise {
         }, 0);
     }
 
-    getPart2Result(): string {
-        return 'Not implemented yet';
+    getPart2Result(): number {
+        const operations: string[] = input.match(/(mul\([0-9]{1,3},[0-9]{1,3}\))|(do\(\))|(don't\(\))/g) ?? [];
+
+        let operationEnable: boolean = true;
+        let acc: number = 0;
+        operations.forEach(
+            (operation: string) => {
+                if (operation === 'do()') {
+                    operationEnable = true;
+                    return;
+                }
+
+                if (operation === 'don\'t()') {
+                    operationEnable = false;
+                    return;
+                }
+
+                if (!operationEnable) {
+                    return;
+                }
+
+                acc = acc + (((operation.match(new RegExp(/[0-9]+/g)) ?? [])
+                    .map((part: string) => +part))
+                    .reduce((prev, curr) => prev * curr, 1)
+                )
+            }
+        )
+
+        return acc;
     }
 }
