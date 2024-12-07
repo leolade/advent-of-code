6fd8cd0fbb5a56cf4e4e06276de29faa0b9479b3
leolade <lad.leo@Hotmail.fr>
2024-12-07 09:55:30 +0100

    Add exercise 7 part 2

--- src/exercises/2024/7/exercise.ts
+++ src/exercises/2024/7/exercise.ts
@@ -1,46 +1,52 @@
 import {Exercise} from '../../exercise';
 import {input} from "./input.ts";
 
+type OperationType = '+' | '*' | '||';
+
 class OperationWrapper {
 
     result: number;
     members: number[];
 
-    constructor(string: string) {
+    constructor(string: string, public operators: OperationType[] = ['+', '*']) {
         this.result = +string.split(':')[0];
         this.members = string.split(':')[1].trim().split(' ').map((n: string) => +n);
     }
 
-    getAllOperatorsCombination(): ('+' | '*')[][] {
-        const results: ('+' | '*')[][] = [];
-        const limit = 2 ** (this.members.length - 1)
+    getAllOperatorsCombination(): OperationType[][] {
+        const results: OperationType[][] = [];
+        const limit = this.operators.length ** (this.members.length - 1)
         for (let i = 0; i < limit; i++) {
-            results.push(i.toString(2).padStart(this.members.length - 1, '0').split('').map((char: string) => char === '0' ? '+' : '*'));
+            results.push(i.toString(this.operators.length).padStart(this.members.length - 1, '0').split('').map((char: string) => this.operators[+char]));
         }
         return results;
     }
 
     match(): boolean {
         return this.getAllOperatorsCombination().some(
-            (operators: ('+' | '*')[]) => {
-                const operations: (number | string)[] = [...this.members];
-                operators.forEach((operator: '+' | '*', index: number) => {
+            (operators: OperationType[]) => {
+                const operations: (number | OperationType)[] = [...this.members];
+                operators.forEach((operator: OperationType, index: number) => {
                     operations.splice((index * 2) + 1, 0, operator);
                 });
                 this.calc(operations);
-                return operations[0] === this.result;
+                return operations.length === 1 && operations[0] === this.result;
             }
         )
     }
 
-    private calc(operations: (number | string)[]) {
-        while (operations.indexOf('*') > -1 || operations.indexOf('+') > -1) {
+    private calc(operations: (number | OperationType)[]) {
+        while ((operations.indexOf('*') > -1 || operations.indexOf('+') > -1 || operations.indexOf('||') > -1) && +operations[0] < this.result) {
             const indexOfMultiplication = operations.indexOf('*');
             const indexOfAddition = operations.indexOf('+');
-            if ((indexOfMultiplication > -1 && indexOfMultiplication < indexOfAddition) || indexOfAddition < 0) {
+            const indexOfConcatenation = operations.indexOf('||');
+            const minValue: number = Math.min(...[indexOfMultiplication, indexOfAddition, indexOfConcatenation].filter((value: number) => value >= 0));
+            if (minValue === indexOfMultiplication) {
                 operations.splice(indexOfMultiplication - 1, 3, (+operations[indexOfMultiplication - 1]) * (+operations[indexOfMultiplication + 1]));
-            } else {
+            } else if (minValue === indexOfAddition) {
                 operations.splice(indexOfAddition - 1, 3, (+operations[indexOfAddition - 1]) + (+operations[indexOfAddition + 1]));
+            } else {
+                operations.splice(indexOfConcatenation - 1, 3, +((operations[indexOfConcatenation - 1] + '') + (operations[indexOfConcatenation + 1] + '')));
             }
         }
     }
@@ -51,16 +57,22 @@ export class Exercise72024 extends Exercise {
     aocYear: number = 2024;
     aocName: string = 'Bridge Repair';
 
-    operations: OperationWrapper[] = input.split('\n').map((line: string) => new OperationWrapper(line));
+    operations: OperationWrapper[] = [];
 
     getPart1Result(): number {
+        this.operations = input.split('\n').map((line: string) => new OperationWrapper(line));
         return this.operations.filter((op: OperationWrapper) => op.match())
             .reduce((prev, curr) => {
             return prev + curr.result
         }, 0)
     }
 
-    getPart2Result(): string {
-        return 'Not implemented yet';
+    getPart2Result(): number {
+        this.operations = input.split('\n').map((line: string) => new OperationWrapper(line, ['+', '*', '||']));
+        const match = this.operations.filter((op: OperationWrapper) => op.match())
+        return match
+            .reduce((prev, curr) => {
+                return prev + curr.result
+            }, 0)
     }
 }
